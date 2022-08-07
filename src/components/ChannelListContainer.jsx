247fbc52cdd79fcce0867ad43f40cc25e2a89b5f
shakhzodbek1997 <55225173+shakhzodbek1997@users.noreply.github.com>
2022-08-07 10:11:08 +0200

    creating button added

--- src/components/ChannelListContainer.jsx
+++ src/components/ChannelListContainer.jsx
@@ -30,7 +30,7 @@ const CompanyHeader = () => (
     </div>
 );
 
-const ChannelListContainer = () => {
+const ChannelListContainer = ({isCreating, setIsCreating, setCreateType,  setIsEditing}) => {
   const logout = () => {
     cookies.remove("token");
     cookies.remove('userId');
@@ -57,6 +57,10 @@ const ChannelListContainer = () => {
                 <TeamChannelList 
                     {...listProps}
                     type="team"
+                    isCreating={isCreating}
+                    setIsCreating={setIsCreating}
+                    setCreateType={setCreateType}  
+                    setIsEditing={setIsEditing}
                 />
             )}
 
@@ -75,6 +79,10 @@ const ChannelListContainer = () => {
              <TeamChannelList 
                  {...listProps}
                  type="messaging"
+                 isCreating={isCreating}
+                 setIsCreating={setIsCreating}
+                 setCreateType={setCreateType}  
+                 setIsEditing={setIsEditing}
              />
          )}
 
