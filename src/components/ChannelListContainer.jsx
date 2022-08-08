7a8fc512c00f8fcd9e384b25a2d3ba82357fd6b5
shakhzodbek1997 <55225173+shakhzodbek1997@users.noreply.github.com>
2022-08-08 16:56:50 +0200

    easy-chatting

--- src/components/ChannelListContainer.jsx
+++ src/components/ChannelListContainer.jsx
@@ -3,7 +3,7 @@ import { ChannelList, useChatContext } from "stream-chat-react";
 import Cookies from "universal-cookie";
 
 import { ChannelSearch, TeamChannelList, TeamChannelPreview } from "./";
-import HospitalIcon from "../assets/hospital.png";
+import Chat from "../assets/Chat.png";
 import LogoutIcon from "../assets/logout.png";
 
 const cookies = new Cookies();
@@ -12,7 +12,7 @@ const SideBar = ({logout}) => (
   <div className="channel-list__sidebar">
     <div className="channel-list__sidebar__icon1">
       <div className="icon1__inner">
-        <img src={HospitalIcon} alt="Hospital" width="30" />
+        <img src={Chat} alt="Hospital" width="30" />
       </div>
     </div>
 
@@ -26,7 +26,7 @@ const SideBar = ({logout}) => (
 
 const CompanyHeader = () => (
     <div className="channel-list__header">
-        <p className="channel-list__header__text">Medical Pager</p>
+        <p className="channel-list__header__text">Easy Chatting</p>
     </div>
 );
 
@@ -131,10 +131,10 @@ const ChannelListContainer = ({ setCreateType, setIsCreating, setIsEditing}) => 
       </div>
 {/* this one is on Mobile version */}
       <div className="channel-list__container-responsive"
-        style={{left: toggleContainer ? "0%" : "-89%", backgroundColor: "#005fff"}}
+        style={{ left: toggleContainer ? "0%" : "-89%", backgroundColor: "#005fff"}}
       >
         <div 
-          className="channel-list__container-toggle" 
+          className="channerl-list__container-toggle" 
           onClick={() => setToggleContainer((prevToggleContainer) => !prevToggleContainer)}>
         </div>
         <ChannelListContent
